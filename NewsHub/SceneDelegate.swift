5d45c7880abb35bcdf8ada5b90fd9a24949703e4
KirillS97 <spielberg080@mail.ru>
2023-11-19 14:55:24 +0300

    Chaged: SceneDelegate
    Добавлена проверка впервые ли запускается приложение + создание папки для хранилища выбранных новостных категорий

--- NewsHub/SceneDelegate.swift
+++ NewsHub/SceneDelegate.swift
@@ -10,9 +10,16 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    private let chosenNewsCategoriesStorageManager: NewsCategoriesStorageManagerProtocol = NewsCategoriesStorageManager.shared
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if self.isTheFirstLaunching() {
+            self.chosenNewsCategoriesStorageManager.save(arrayOfCategories: [])
+            print("This is first launching")
+        }
+        
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -30,6 +37,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
     
+    // MARK: Проверка впервые ли запускается приложение
+    private func isTheFirstLaunching() -> Bool {
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "theFirstLaunchWasCompleted") == nil {
+            userDefaults.setValue(true, forKey: "theFirstLaunchWasCompleted")
+            return true
+        } else {
+            return false
+        }
+    }
     
 }
 
